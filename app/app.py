f6c68434697a9085f5f5e34d9447fa8883e25b3b
georggie <krivokapic.djordjije@gmail.com>
2019-10-26 21:05:33 +0200

    Quick fix

--- app/app.py
+++ app/app.py
@@ -2,13 +2,13 @@ from boostrap.bootstraper import App
 from utility.generator import Generator
 from utility.query_analyzer import QueryAnalyzer
 
-# generator = Generator()  # Generator of data and queries
-# generator.generate_data()
-# generator.generate_queries()
+generator = Generator()  # Generator of data and queries
+generator.generate_data()
+generator.generate_queries()
 
 application = App()  # Boostrap TPC-DS banchmark
-# application.create_database()  # Set up new database
-# application.preprocess_queries()  # Preprocess queries before running
+application.create_database()  # Set up new database
+application.preprocess_queries()  # Preprocess queries before running
 
 query_analzer = QueryAnalyzer(application.spark)  # Get an instance of query analyzer
 query_analzer.run_benchmark()  # Finally run a banchmark
